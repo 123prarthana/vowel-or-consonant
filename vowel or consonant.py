46e59cb05f4de63d46bad935a269f4edd3348b03
123prarthana <130730475+123prarthana@users.noreply.github.com>
2023-05-14 14:06:02 +0530

    Add files via upload

---
+++ vowel or consonant.py
@@ -0,0 +1,8 @@
+c= input("enter the character: ")
+if c=='a' or c=='e' or c=='i' or c=='o' or c=='u':
+    print("vowel")
+elif c=='A' or c=='E' or c=='I' or c=='O' or c=='U':
+    print("vowel")
+else:
+    print("consonant")
+    
